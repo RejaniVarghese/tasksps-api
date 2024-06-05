72d251563aa0b2e9db6ccfd3e0d1ee318bb505ed
RejaniVarghese <rejanimv@yahoo.co.in>
2024-06-05 07:29:14 +0530

    cors converted to *

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors({
 }));
 
 app.all('/*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tasksps-client.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
